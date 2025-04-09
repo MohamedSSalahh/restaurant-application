d79908e35225fabe0fc52d03da8e7b8d5440922a
mohamedsalah <hsalahxxx@gmail.com>
2025-04-10 00:51:03 +0200

    "done"

--- controller/reservation.js
+++ controller/reservation.js
@@ -1,1 +1,30 @@
-import ErrorHandler from "../error/error.js";
+import { ErrorHandler } from "../error/error.js";
+import {Reservation} from "../models/resrvationSchame.js";
+
+
+export const sendReservation = async (req, res, next) => {
+    const { firstname, lastname, email, phone, time, date } = req.body;
+    if (!firstname || !lastname || !email || !phone || !time || !date) {
+        return next(new ErrorHandler("Please provide all fields", 400));
+    }
+    try {
+         await Reservation.create({
+            firstname,
+            lastname,
+            email,
+            phone,
+            time,
+            date,
+        });
+        return res.status(201).json({
+            success: true,
+            message: "Reservation created successfully",
+            
+        });
+    } catch (error) {
+        if (error.name === "ValidationError") {
+           const validatorErrors = Object.values(error.errors).map((err)=> err.message)
+           return next(new ErrorHandler(validatorErrors.join(", "), 400));
+        }
+    }
+}
--- error/error.js
+++ error/error.js
@@ -1,16 +1,24 @@
+// error.js
+
 class ErrorHandler extends Error {
     constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-        Error.captureStackTrace(this, this.constructor);
-  }}
-  export const errorMIddleware = (err, req, res, next) =>{
-err.message = err.message || "Internal Server Error";
-err.statusCode = err.statusCode || 500;
-return res.status(err.statusCode).json({
-    success: false,
-    message: err.message,
-    statusCode: err.statusCode,
-    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,  
-})
-}
+      super(message);
+      this.statusCode = statusCode;
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  export { ErrorHandler };
+  
+  export const errorMIddleware = (err, req, res, next) => {
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+    
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      statusCode: err.statusCode,
+      stack: process.env.NODE_ENV === "development" ? err.stack : undefined,  
+    });
+  };
+  
